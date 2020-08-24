8ea33d875b1790d047d470000cda0f8f54fe08d2
Pedro <windowsxpedro@gmail.com>
2020-08-23 21:16:03 -0300

    initial

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import SearchBar from './SearchBar'
+import VideoDetail from './VideoDetail'
+import VideoList from './VideoList'
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>App</div>
+        )
+    }
+}
+
+export default App
---
+++ src/components/VideoList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import VideoItem from './VideoItem'
+
+
+class VideoList extends React.Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default VideoList

dc2d1f11b1a59e7579b0d6c681585bd194555aee
Pedro <windowsxpedro@gmail.com>
2020-08-28 15:38:46 -0300

    Update README

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -4,8 +4,6 @@ import React from 'react'
 
 const VideoItem = ({onClick, video}) => {
 
-    const videoTitle = video.snippet.title
-    const videoUrl =`https://youtube.com/watch?v=${video.id.videoId}`
     const channelUrL = `https://www.youtube.com/channel/${video.snippet.channelId}`
 
     const handleClick = (video) => {
@@ -21,7 +19,11 @@ const VideoItem = ({onClick, video}) => {
             src={video.snippet.thumbnails.medium.url}
             />
             <div className="content">
-            <div className="header">{video.snippet.title}</div>
+                <div className="header">
+                    <h4>{video.snippet.title}</h4>
+                    <a href={channelUrL}>{video.snippet.channelName}</a>
+
+                </div>
             </div>
         </div> 
     )
